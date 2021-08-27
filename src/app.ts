8e88b4c84ecc8b72ea3aa48ea0daad806c94c3f7
Bruno Frascino <bfrascino80@gmail.com>
2021-08-27 23:10:16 +1000

    chore: Nodemon and env config file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 /* eslint-disable @getify/proper-arrows/where */
 import express, { Application, Request, Response } from 'express';
+import { defaultConfig } from './config/default-config';
 
+const { port, host } = defaultConfig;
 const app: Application = express();
-const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req: Request, res: Response) => res.send('<h2>Express + TypeScript Server + Nodemon</h2>'));
 
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+app.listen(port, host, () => {
+  console.log(`⚡️[server]: Server is running at http://${host}:${port}`);
 });
---
+++ src/config/default-config.ts
@@ -0,0 +1,5 @@
+export const defaultConfig = {
+  port: 3000,
+  host: 'localhost',
+  dbUri: 'something',
+};
