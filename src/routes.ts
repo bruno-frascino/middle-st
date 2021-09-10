4c863d36d36234b1174704b2ca07f4221faadce2
Bruno Frascino <bfrascino80@gmail.com>
2021-09-11 00:22:34 +1000

    fix: missing route and cleanup

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Express, Request, Response } from 'express';
 
 export default function (app: Express) {
+  app.get('/', (req: Request, res: Response) => res.send('<h2>Express + TypeScript Server + Nodemon</h2>'));
+
   app.get('/health', (req: Request, res: Response) => {
     res.sendStatus(200);
   });
