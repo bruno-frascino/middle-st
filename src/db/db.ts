aa971c2c0d93ae6c244e8cf8a362855924e4df94
Bruno Frascino <bfrascino80@gmail.com>
2021-09-21 23:35:57 +1000

    feat: New Db functionalities

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,6 +2,7 @@ import config from 'config';
 import sqlite3 from 'sqlite3';
 import log from '../logger';
 import { Integration } from '../model/db.model';
+import { Notification } from '../model/tray.model';
 
 let db: sqlite3.Database;
 
@@ -43,6 +44,21 @@ export function getRow(sql: string, params: any[] = []) {
   });
 }
 
+export function run(sql: string, params: {} = {}) {
+  log.info(`Running sql: ${sql} with params: ${JSON.stringify(params)}`);
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line func-names
+    getInstance().run(sql, params, function (err) {
+      if (err) {
+        log.error(`Error running sql: ${sql} with error ${err}`);
+        reject(err);
+      } else {
+        resolve({ id: this.lastID });
+      }
+    });
+  });
+}
+
 export async function getIntegration(sellerId: number, appCode: string) {
   const sql = `SELECT * 
               FROM INTEGRATION 
@@ -50,4 +66,20 @@ export async function getIntegration(sellerId: number, appCode: string) {
               AND sellerTStoreCode = ?`;
 
   return (await getRow(sql, [sellerId, appCode])) as Integration;
+}
+
+export async function insertNotification(notification: Notification) {
+  const sql = `INSERT INTO NOTIFICATION(
+              id, scopeName, act, scopeId, sellerId, appCode, createDate) 
+              VALUES(
+              null, $scopeName, $act, $scopeId, $sellerId, $appCode, strftime('%s','now')
+            );`;
+
+  return (await run(sql, {
+    $scopeName: notification.scope_name,
+    $act: notification.act,
+    $scopeId: notification.scope_id,
+    $sellerId: notification.seller_id,
+    $appCode: notification.app_code,
+  })) as Object;
 }
