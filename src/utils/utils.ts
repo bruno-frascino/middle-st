761dc0f64ec4016ad718511e13ef1a9ddf4a35c3
Bruno Frascino <bfrascino80@gmail.com>
2021-10-13 21:36:38 +1100

    feat: Util fn date

---
+++ src/utils/utils.ts
@@ -0,0 +1,16 @@
+// 2021-04-01 11:58:21
+export function convertStringToUnixTime(dateString: string) {
+  const two = dateString.split(' ');
+  const date = two[0].split('-');
+  const time = two[1].split(':');
+  // year, month, day, hour, minute, second
+  const dateTimeObject = new Date(
+    parseInt(date[0], 10),
+    parseInt(date[1], 10),
+    parseInt(date[2], 10),
+    parseInt(time[0], 10),
+    parseInt(time[1], 10),
+    parseInt(time[2], 10),
+  );
+  return Math.floor(dateTimeObject.getTime() / 1000);
+}
