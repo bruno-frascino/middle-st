5e5f7bfd04c64ff9d9ad146f656aa0b8e03e7e43
Bruno Frascino <bfrascino80@gmail.com>
2023-10-03 18:17:50 -0300

    feat: Tidy up file and improve db schema

--- script/db_schema_mysql.sql
+++ script/db_schema_mysql.sql
@@ -2,23 +2,21 @@
 -- S and T Integration Details
 CREATE TABLE Integration(
   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-  sellerName VARCHAR(60),
-  sellerSId INT UNSIGNED, 
+  sellerName VARCHAR(60), 
   sellerSKey VARCHAR(255), 
   sellerSSecret VARCHAR(255),
   sellerSAccessToken VARCHAR(600),
   sellerSRefreshToken VARCHAR(600),
-  sellerSAccessExpirationDate SMALLINT UNSIGNED,  -- unix time
-  sellerSRefreshExpirationDate SMALLINT UNSIGNED,
-  -- sellerTId INT UNSIGNED UNIQUE NOT NULL, 
-  sellerTStoreCode VARCHAR(30) UNIQUE NOT NULL,
-  sellerTStoreAccessCode VARCHAR(30),
+  sellerSAccessExpirationDate INT UNSIGNED,  -- unix time
+  sellerSRefreshExpirationDate INT UNSIGNED,
+  sellerTStoreCode VARCHAR(90) UNIQUE NOT NULL,
+  sellerTStoreAccessCode VARCHAR(100),
   sellerTStoreAdminUser VARCHAR(30),
-  sellerTStoreUrl VARCHAR(60),
+  sellerTStorePath VARCHAR(90),
   sellerTAccessToken VARCHAR(600),
   sellerTRefreshToken VARCHAR(600),
-  sellerTAccessExpirationDate DATETIME,  -- YYYY-MM-DD hh:mm:ss
-  sellerTRefreshExpirationDate DATETIME, -- YYYY-MM-DD hh:mm:ss
+  sellerTAccessExpirationDate INT UNSIGNED,  -- unix time
+  sellerTRefreshExpirationDate INT UNSIGNED, -- unix time
   createDate DATETIME NOT NULL,
   active BOOLEAN NOT NULL
 );
@@ -39,12 +37,6 @@ CREATE TABLE Notification(
       REFERENCES Integration(id)
       ON DELETE CASCADE
 );
---  -- Middleware T Credentials
--- CREATE TABLE T_CREDENTIALS(
---   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
---   'key' text NOT NULL,
---   secret text NOT NULL
--- );
 
 -- Integration x T Product x S Product
 CREATE TABLE IProduct(
@@ -71,67 +63,6 @@ CREATE TABLE IProduct_SKU(
   updateDate DATETIME, 
   state CHAR(1) NOT NULL, -- [C,U,D] CREATED, UPDATED, DELETED
   FOREIGN KEY(iProductId) 
-    REFERENCES IPRODUCT(id) 
+    REFERENCES IProduct(id) 
     ON DELETE CASCADE
 );
-
--- Load
-INSERT INTO INTEGRATION(
-  id,
-  sellerName, 
-  sellerSId, 
-  sellerSKey, 
-  sellerSSecret, 
-  -- sellerSStoreCode, 
-  -- sellerSStoreUrl,
-  -- sellerSAccessToken,
-  -- sellerSRefreshToken,
-  -- sellerSAccessExpirationDate,
-  -- sellerSRefreshExpirationDate,
-  sellerTId, 
-  -- sellerTKey, 
-  -- sellerTSecret, 
-  sellerTStoreCode, 
-  sellerTStoreUrl,
-  -- sellerTAccessToken,
-  -- sellerTRefreshToken,
-  -- sellerTAccessExpirationDate,
-  -- sellerTRefreshExpirationDate,
-  active
-) 
-VALUES(
-  1,
-  'Loja Test 1',
-  1,
-  'Iki7Q2W3xS7UZw0kNHvsGnmMpAE0tZ35',
-  'uvL0Ksh2TKgFJKdKiXdVGiUuD0IRwsam',
-  1, 
-  '1225878',
-  'Tray Url 1',
-  1
-  );
-
-INSERT INTO NOTIFICATION VALUES(null, 'scope', 'act', 99, strftime('%s','now'));
-
-
-SELECT datetime(d1,'unixepoch')
-FROM datetime_int;
-
--- Read
--- SELECT * FROM SM_USER WHERE id=1;
-SELECT * FROM INTEGRATION;
-
--- Update Table definition
-ALTER TABLE sharks ADD COLUMN age integer;
-
--- Update Values in a table
-UPDATE SM_USER SET key = 'NEW KEY' WHERE id = 1;
-
--- Delete Values
-DELETE FROM SM_USER WHERE id = 1;
-
--- Delete Table
-DROP TABLE SM_USER;
-DROP TABLE INTEGRATION;
-DROP TABLE T_CREDENTIALS;
-
